5361c1aa845ed862c063be548036bdce9d7b16d1
“zyucar” <zeynep.ucar016@gmail.com>
2015-03-20 21:22:38 +0200

    ng-hide

---
+++ Angularjs/sistem.js
@@ -0,0 +1,14 @@
+var app = angular.module('myApp', []);
+	app.controller("control",function($scope) {
+		$scope.toggle = true;
+		$scope.toggl = true;
+		$scope.togg = true;
+		$scope.tog = true;
+		$scope.to = true;
+		$scope.t = true;
+		//$scope.toggle = function() {
+
+			//$scope.myvar = $scope.myvar === false ? true: false;
+
+		//};
+	});
